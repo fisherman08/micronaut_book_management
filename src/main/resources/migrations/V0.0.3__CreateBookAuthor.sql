9d14b5e76f8b2616f836bfe8eb96fd9ac08508ec
kaneko <orenoikizama@hotmail.com>
2021-01-30 14:38:34 +0900

    feat: BookAuthorのDDL

---
+++ src/main/resources/migrations/V0.0.3__CreateBookAuthor.sql
@@ -0,0 +1,9 @@
+CREATE TABLE book_author (
+  book_id BINARY(16) NOT NULL,
+  author_id BINARY(16) NOT NULL,
+  PRIMARY KEY (book_id, author_id),
+  CONSTRAINT fk_ba_book_id_to_book
+    FOREIGN KEY (book_id) REFERENCES book (id) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT fk_ba_author_id_to_book
+    FOREIGN KEY (author_id) REFERENCES writer (id) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
