aa1ecb978e49766d3cd189d967692374272b7fe9
kaneko <orenoikizama@hotmail.com>
2021-01-31 14:20:54 +0900

    refactor: idのデータ型を変更

--- src/main/resources/migrations/V0.0.3__CreateBookAuthor.sql
+++ src/main/resources/migrations/V0.0.3__CreateBookAuthor.sql
@@ -1,6 +1,6 @@
 CREATE TABLE book_author (
-  book_id BINARY(16) NOT NULL,
-  author_id BINARY(16) NOT NULL,
+  book_id VARCHAR(36) NOT NULL,
+  author_id VARCHAR(36) NOT NULL,
   PRIMARY KEY (book_id, author_id),
   CONSTRAINT fk_ba_book_id_to_book
     FOREIGN KEY (book_id) REFERENCES book (id) ON DELETE CASCADE ON UPDATE NO ACTION,
