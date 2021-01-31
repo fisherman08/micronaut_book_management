67cabbf9522581316310b1a2b7f42f5e3009d1fb
kaneko <orenoikizama@hotmail.com>
2021-01-31 19:59:59 +0900

    feat: WriterRepositoryの定義

---
+++ src/main/kotlin/com/github/fisherman08/micronautbooks/domain/writer/WriterRepository.kt
@@ -0,0 +1,8 @@
+package com.github.fisherman08.micronautbooks.domain.writer
+
+interface WriterRepository {
+    fun listAll(): List<Writer>
+    fun find(writerId: WriterId): Writer
+    fun save(writer: Writer): Writer
+    fun delete(writerId: WriterId)
+}
