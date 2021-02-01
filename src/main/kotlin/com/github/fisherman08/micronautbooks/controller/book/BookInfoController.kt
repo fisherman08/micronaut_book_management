b79a276be8afa10ac66fbc512215ba6e5253f741
kaneko <orenoikizama@hotmail.com>
2021-02-01 19:46:29 +0900

    feat: bookinfocondtollerを修正

--- src/main/kotlin/com/github/fisherman08/micronautbooks/controller/book/BookInfoController.kt
+++ src/main/kotlin/com/github/fisherman08/micronautbooks/controller/book/BookInfoController.kt
@@ -1,5 +1,6 @@
 package com.github.fisherman08.micronautbooks.controller.book
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.github.fisherman08.micronautbooks.controller.ApiPaths
 import com.github.fisherman08.micronautbooks.domain.book.BookId
 import com.github.fisherman08.micronautbooks.usecase.book.BookInfoUseCase
@@ -17,8 +18,16 @@ class BookInfoController(
         @PathVariable id: String
     ): ResponseBody {
         val book = getInfo(BookId.fromString(id))
-        return ResponseBody(id = book.id.value, title = book.title.value)
+
+        return ResponseBody(
+            id = book.id.value,
+            title = book.title.value,
+            authors = book.authors.map { au -> ResponseBody.Author(id = au.id.value, name = au.name.value) }
+        )
     }
 
-    data class ResponseBody(val id: UUID, val title: String)
+    @JsonInclude(JsonInclude.Include.ALWAYS)
+    data class ResponseBody(val id: UUID, val title: String, val authors: List<Author>) {
+        data class Author(val id: UUID, val name: String)
+    }
 }
